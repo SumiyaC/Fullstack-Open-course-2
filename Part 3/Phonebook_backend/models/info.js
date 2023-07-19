58768fb316ac3da4b59368630282d6ce28d6b005
Sumiya Chowdhury <sumiyachowdury184@gmail.com>
2023-07-19 14:54:19 +0300

    phonebook(3.13-3.14)

---
+++ Part 3/Phonebook_backend/models/info.js
@@ -0,0 +1,115 @@
+const mongoose = require('mongoose');
+
+const url = process.env.MONGODB_URI;
+console.log('connecting to', url);
+
+mongoose
+  .connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    console.log('connected to MongoDB');
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message);
+  });
+
+
+const phonebookSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+});
+
+phonebookSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+const Person = mongoose.model('Person', phonebookSchema);
+module.exports = Person;
+
+if (process.argv.length === 3) {
+  // Fetch all entries in the phonebook
+  console.log('phonebook:')
+  Person.find({}).then((persons) => {
+    persons.forEach((person) => {
+      console.log(`${person.name} ${person.number}`)
+    });
+    mongoose.connection.close()
+  });
+} else if (process.argv.length === 5) {
+  // Add a new entry to the phonebook
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const person = new Person({
+    name,
+    number,
+  });
+
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })}
+
+// const mongoose = require('mongoose')
+
+// mongoose.set('strictQuery', false)
+
+// const url = process.env.MONGODB_URI
+
+// console.log('connecting to', url)
+
+// mongoose.connect(url)
+
+//   .then(result => {
+//     console.log('connected to MongoDB')
+//   })
+//   .catch((error) => {
+//     console.log('error connecting to MongoDB:', error.message)
+//   })
+
+//   const phonebookSchema = new mongoose.Schema({
+//     name: String,
+//     number: String,
+//   });
+
+//   phonebookSchema.set('toJSON', {
+//   transform: (document, returnedObject) => {
+//     returnedObject.id = returnedObject._id.toString()
+//     delete returnedObject._id
+//     delete returnedObject.__v
+//   }
+// })
+// module.exports = mongoose.model('ppl', phonebookSchema)
+// const Person = mongoose.model('Person', phonebookSchema)
+
+// if (process.argv.length === 3) {
+//     // Fetch all entries in the phonebook
+//     console.log('phonebook:')
+//     Person.find({}).then((persons) => {
+//       persons.forEach((person) => {
+//         console.log(`${person.name} ${person.number}`)
+//       });
+//       mongoose.connection.close()
+//     });
+//   } else if (process.argv.length === 5) {
+//     // Add a new entry to the phonebook
+//     const name = process.argv[3]
+//     const number = process.argv[4]
+  
+//     const person = new Person({
+//       name,
+//       number,
+//     });
+  
+//     person.save().then(() => {
+//       console.log(`added ${name} number ${number} to phonebook`)
+//       mongoose.connection.close()
+//     });
+//   } else {
+//     console.log('Invalid command-line arguments.')
+//     mongoose.connection.close()
+//   }
+  
--- Part 3/Phonebook_backend/mongo.js
+++ Part 3/Phonebook_backend/mongo.js
@@ -39,3 +39,45 @@ if (process.argv.length === 3) {
   console.log('Invalid command-line arguments.')
   mongoose.connection.close()
 }
+
+// const mongoose = require('mongoose')
+
+// const password = process.argv[2]
+// const url =`mongodb+srv://fullstack:${password}@cluster0.vnfg6td.mongodb.net/phonebookDB?retryWrites=true&w=majority`
+
+// mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+
+// const phonebookSchema = new mongoose.Schema({
+//   name: String,
+//   number: String,
+// });
+
+// const Person = mongoose.model('Person', phonebookSchema)
+
+// if (process.argv.length === 3) {
+//   // Fetch all entries in the phonebook
+//   console.log('phonebook:')
+//   Person.find({}).then((persons) => {
+//     persons.forEach((person) => {
+//       console.log(`${person.name} ${person.number}`)
+//     });
+//     mongoose.connection.close()
+//   });
+// } else if (process.argv.length === 5) {
+//   // Add a new entry to the phonebook
+//   const name = process.argv[3]
+//   const number = process.argv[4]
+
+//   const person = new Person({
+//     name,
+//     number,
+//   });
+
+//   person.save().then(() => {
+//     console.log(`added ${name} number ${number} to phonebook`)
+//     mongoose.connection.close()
+//   });
+// } else {
+//   console.log('Invalid command-line arguments.')
+//   mongoose.connection.close()
+// }
